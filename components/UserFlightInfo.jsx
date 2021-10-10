58f2f8f1765700054f52cb9257259c09aebd9780
Christopher Woodby <chrisdwoodby@gmail.com>
2021-10-10 01:01:06 -0500

    Update UserFlightInfo.jsx

--- components/UserFlightInfo.jsx
+++ components/UserFlightInfo.jsx
@@ -11,7 +11,7 @@ var UserFlightInfo = function(props) {
 
   var getUserFlightData = function() {
     props.setLoading(true);
-    axios.get('http://3.138.60.186/trips', {params: {userId: props.userID}})
+    axios.get('http://18.219.7.64/trips', {params: {userId: props.userID}})
     .then((response) => {
       console.log(response);
       setTrips(response.data);
