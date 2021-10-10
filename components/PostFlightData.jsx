7680a35765fd874e1adb5f443d47c5fd39a5046d
Christopher Woodby <chrisdwoodby@gmail.com>
2021-10-10 01:00:44 -0500

    Update PostFlightData.jsx

--- components/PostFlightData.jsx
+++ components/PostFlightData.jsx
@@ -31,7 +31,7 @@ var PostFlightData = function(props) {
       airline: airline,
       userId: props.userID
     };
-    axios.post('http://3.138.60.186/trips', tripObj)
+    axios.post('http://18.219.7.64/trips', tripObj)
     .then((response) => {
       console.log(response);
     })
