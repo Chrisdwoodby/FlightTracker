68f61ae0ffc223d4f75e14e71cef3256ab2116bf
Christopher Woodby <chrisdwoodby@gmail.com>
2021-10-17 15:45:35 -0500

    Update UserFlightInfo.jsx

--- components/UserFlightInfo.jsx
+++ components/UserFlightInfo.jsx
@@ -11,7 +11,7 @@ var UserFlightInfo = function(props) {
 
   var getUserFlightData = function() {
     props.setLoading(true);
-    axios.get('http://52.14.88.6/trips', {params: {userId: props.userID}})
+    axios.get('https://cdwflighttracker.herokuapp.com/trips', {params: {userId: props.userID}})
     .then((response) => {
       console.log(response);
       setTrips(response.data);
