7a15ffa3bded4a11ea564b9d189a898e9872a52f
Christopher Woodby <chrisdwoodby@gmail.com>
2021-10-18 19:52:53 -0500

    Update ExistingUser.jsx

--- components/ExistingUser.jsx
+++ components/ExistingUser.jsx
@@ -23,7 +23,7 @@ var ExistingUser = function(props) {
   };
 
   var authenticate = function() {
-    axios.get('http://18.190.99.131/users', {params: {userName: props.signedIn, userPassword: encrypted}})
+    axios.get('http://us-cdbr-east-04.cleardb.com/users', {params: {userName: props.signedIn, userPassword: encrypted}})
     .then((response) => {
       console.log(response);
       setDisplayName(`Welcome ${response.data[0].firstName}!`);
