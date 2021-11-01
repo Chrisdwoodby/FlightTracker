d893ddea2739a83106bff74d56f2078b51725a2e
Christopher Woodby <chrisdwoodby@gmail.com>
2021-11-01 13:30:26 -0500

    Update HomePage.jsx

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -23,13 +23,15 @@ function HomePage(props) {
             CDW Global was established with the goal to simplify your air travel experience by providing an easy to navigate interface for flight tracking that anyone and everyone can use. We home to provide our users with the most accurate and up to date information to present a greater sense of relaxation and assurance in our stress filled world.
           </p>
           <h3 style={{padding: "20px", fontFamily: "sans-serif", textTransform: "uppercase", letterSpacing: "2px"}}>About the creator</h3>
+          <h3 style={{padding: "20px", fontFamily: "sans-serif", textTransform: "uppercase", letterSpacing: "2px", opacity: "0.75"}}>Christopher Woodby</h3>
             <Row>
               <Col md="auto">
                 <Image src={download} width="200" height="200" style={{padding: "15px", borderRadius: "50%"}}/>
               </Col>
               <Col>
                 <Row>
-                 <p style={{padding: "15px", fontFamily: "sans-serif"}}>Christopher Woodby, a Hack Reactor Software Engineering Graduate with 5 years of experience working in client-facing environments, leading and managing personnel through strategic collaboration to continuously improve customers' experience and their satisfaction with the organization. Experienced in developing innovative solutions to more effectively meet clients' expectations through cutting edge product development and data management. A people first oriented work ethic, and determined to utilize the tools of today to engineer a better tomorrow. </p>
+                 <p style={{padding: "15px", fontFamily: "sans-serif"}}>
+                   “My passion for design has driven me to build the product I desire to produce without compromise. Every web page should be designed in a similar manner to that of a modern car, for example if a person were to get into a vehicle for the first time, it can be assumed that this person would still be able to operate the vehicle without difficulty. The same standard should exist for modern web pages, while at the same time providing a unique and memorable experience for the user.”</p>
                 </Row>
               </Col>
             </Row>
